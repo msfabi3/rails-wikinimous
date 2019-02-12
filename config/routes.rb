b6b243182f394b23238f1f919d828373a36be696
Fabiana Carvalho <fabiii.fa@gmail.com>
2019-02-12 17:00:27 -0200

    solving rails wikinimous

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  get 'articles', to: 'articles#index'
+  get 'articles/new', to: 'articles#new', as: 'new'
+  post 'articles', to: 'articles#create'
+  get 'articles/:id', to: 'articles#show', as: 'article'
+  get 'articles/:id/edit', to: 'articles#edit', as: 'edit_article'
+  patch 'articles/:id', to: 'articles#update'
+  delete 'articles/:id', to: 'articles#destroy'
+
+  root to: 'articles#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
